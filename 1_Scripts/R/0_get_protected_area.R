99194f68301d8d0f2fa0f34a4f0a512db3b934f8
VanessaDimaurizio <vanessadimaurizio@gmail.com>
2024-05-15 18:21:06 -0400

    Change protected areas 2022 to 2023

--- 1_Scripts/R/0_get_protected_area.R
+++ 1_Scripts/R/0_get_protected_area.R
@@ -1,8 +1,8 @@
 # Specify the URL of the file you want to download
-url <- "https://data-donnees.az.ec.gc.ca/api/file?path=/species%2Fprotectrestore%2Fcanadian-protected-conserved-areas-database%2FDatabases%2FAireProtegeeConservee_2022.gdb.zip"
+url <- "https://data-donnees.az.ec.gc.ca/api/file?path=/species%2Fprotectrestore%2Fcanadian-protected-conserved-areas-database%2FDatabases%2FProtectedConservedArea_2023.zip"
 
 # Specify the file name and location where you want to save the file on your computer
-file_name <- "proteced_areas.gdb"
+file_name <- "protected_areas.gdb"
 file_path <- "0_Data/"
 
 # Call the download.file() function, passing in the URL and file name/location as arguments
