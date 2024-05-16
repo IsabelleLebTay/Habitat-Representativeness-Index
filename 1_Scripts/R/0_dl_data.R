6c7fdacb6b305af0ec239c5138f1eacbdb01a4a4
BenMerSci <benjamin.b.mercier@usherbrooke.ca>
2024-05-16 18:32:07 -0400

    Changed download file

--- 1_Scripts/R/0_dl_data.R
+++ 1_Scripts/R/0_dl_data.R
@@ -10,30 +10,13 @@ dl_ecozones <- function(filename = import_config()$ecozones_zipfile,
   url <- "https://sis.agr.gc.ca/cansis/nsdb/ecostrat/zone/ecozone_shp.zip"
 
   # Create directory if it doesn't exist
-  chk_dir(datadir)
+  chk::chk_dir(datadir)
   # Call the download.file() function, passing in the URL and file name/location as arguments
   download.file(url, sprintf("%s/%s.zip", tempdir(), filename), mode = "wb")
 
   # Unzip the folder
   unzip(sprintf("%s/%s.zip", tempdir(), filename), exdir = here::here(datadir))
 
-}
-
-dl_bioclim_data <- function(filename = import_config()$ecozones_zipfile,
-                            datadir = import_config()$raw_data_path) {
-  
-  # Download the ecozone data
-  # Specify the URL of the file you want to download
-  url <- "https://diffusion.mffp.gouv.qc.ca/Diffusion/DonneeGratuite/Foret/DONNEES_FOR_ECO_SUD/Classification_ecologique/CLASSI_ECO_QC_GDB.zip"
-  
-  # Create directory if it doesn't exist
-  chk_dir(datadir)
-  # Call the download.file() function, passing in the URL and file name/location as arguments
-  download.file(url, sprintf("%s/%s.zip", tempdir(), filename), mode = "wb")
-  
-  # Unzip the folder
-  unzip(sprintf("%s/%s.zip", tempdir(), filename), exdir = here::here(datadir))
-  
 }
 
 #' Download and extract protected areas 
@@ -48,7 +31,7 @@ dl_protected_areas <- function(filename = import_config()$protected_zipfile,
   url <- "https://data-donnees.az.ec.gc.ca/api/file?path=/species%2Fprotectrestore%2Fcanadian-protected-conserved-areas-database%2FDatabases%2FAireProtegeeConservee_2023.zip"
 
   # Create directory if it doesn't exist
-  chk_dir(datadir)
+  chk::chk_dir(datadir)
   # Call the download.file() function, passing in the URL and file name/location as arguments
   download.file(url, sprintf("%s/%s.zip", tempdir(), filename), mode = "wb")
 
