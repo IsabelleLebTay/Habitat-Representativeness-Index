f56a99ddb45f7d4feaf74ecffc886ab43efae8bf
Isabelle Lebeuf-Taylor <lebeufta@ualberta.ca>
2024-05-15 21:01:34 -0400

    modularising functions and tests

--- 1_Scripts/R/4_evenness_trend.R
+++ 1_Scripts/R/4_evenness_trend.R
@@ -3,7 +3,8 @@
 source("1_Scripts/R/0_get_ecozone.R")
 source("1_Scripts/R/0_get_protected_area.R")
 source("1_Scripts/R/1_compute_prop_protected_areas.R")
-source("1_Scripts/R/1_filter_protection_level.R")
+source("1_Scripts/R/2_filter_protection_level.R")
 
 eco_areas_file_name -> get_ecozone()
 protected_areas_file_name -> get_protected_areas()
+
